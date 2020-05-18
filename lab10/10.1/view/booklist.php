b313759a6322c8d66595eba36f13af911c1faa0a
Pham Hong Quang 20163316 <quang.ph163316@sis.hust.edu.vn>
2020-05-18 22:52:51 +0700

    week 9

--- lab10/10.1/view/booklist.php
+++ lab10/10.1/view/booklist.php
@@ -10,7 +10,7 @@
             </tbody>
             <?php
             foreach ($book as $title => $book) {
-                echo '<tr><td><a href="index.php?book=' . $book->title . '">' . $book->title . '</a></td><td>' . $book->author . '</td><td>' . $book->description . '</td></tr>';
+                echo '<tr><td><a href="index.php?book='.$book->title.'">'.$book->title.'</a></td><td>'.$book->author.'</td><td>'.$book->description.'</td></tr>';
             }
             ?>
         </table>
--- lab10/index.php
+++ lab10/index.php
@@ -1,4 +1,8 @@
 <?php
-include_once("controller/Controller.php");
-$controller = new Controller();
-$controller->invoke();
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(dirname(__FILE__)));
+
+$url = $_GET['url'];
+
+require_once(ROOT . DS . 'library' . DS . 'bootstrap.php');
